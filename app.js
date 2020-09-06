66107afdc686406ba721ef13ba3ec2ed5a878532
lio2011 <56246301+lio2011@users.noreply.github.com>
2020-09-07 00:40:13 +0530

    b

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 400,
         "density": {
           "enable": true,
           "value_area": 800
